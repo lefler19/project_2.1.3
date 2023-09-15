66a5515f91e567036929aae66765a96acb276ca6
Настя <west_2017@mail.ru>
2023-09-15 18:48:29 +0300

    2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,19 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+
+
+
+        System.out.println("First result " + (bean == bean2));
+        System.out.println("Second result " + (cat == cat2));
+
     }
+
 }
